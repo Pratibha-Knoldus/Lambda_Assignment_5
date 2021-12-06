9816940a560182ff78b50006fdd820ba9ca0fc1d
Pratibha-Knoldus <pratibha.yadav@knoldus.com>
2021-12-06 10:38:41 +0530

    done

---
+++ src/DistinctStrings.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+interface DistinctStringsInterface{
+    String viewStr(List<String> str);
+
+}
+
+public class DistinctStrings {
+    public static void main(String...args){
+        DistinctStringsInterface strObj = (str) -> {
+            str = new ArrayList<>();
+            str.add("java");
+            str.add("scala");
+            str.add("java");
+            str.add("clojure");
+            str.add("clojure");
+            str.add("scala");
+            List<String> list = str.stream().distinct().collect(Collectors.toList());
+
+            return String.valueOf(list);
+        };
+        System.out.println(strObj.viewStr(Arrays.asList()));
+    }
+}
